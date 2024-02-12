8a78ec65dbe0dee975debf28ea52d73af53b42e4
CEO-Niels <nielsbeaumont.etu@gmail.com>
2024-02-12 16:07:24 +0100

    page home, cercle ajout fleche + bloc bleu en dessous

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -14,87 +14,115 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              child: Stack(
-                children: [
-                  Center(
-                    child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(
-                          colors: [Color(0xFF004396), Color(0xFF0C8CE9)],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
+
+      //cercle glycemique
+      body: Column(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            child: Stack(
+              children: [
+                Center(
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF004396), Color(0xFF0C8CE9)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 185,
+                    height: 185,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Text(
+                        '6.2', // donnée fictive en chiffre
+                        style: TextStyle(
+                          color: Color(0xFF004396),
+                          fontSize: 64,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
-                  Center(
-                    child: Container(
-                      width: 185,
-                      height: 185,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      child: Center(
-                        child: Text(
-                          '123', // donnée fictive en chiffre
-                          style: TextStyle(
-                            color: Color(0xFF004396),
-                            fontSize: 64,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                ),
+                Positioned(
+                  bottom: 45,
+                  left: 90,
+                  child: Container(
+                    width: 100,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
                     ),
-                  ),
-                  Positioned(
-                    bottom: 45,
-                    left: 90, // Déplacement vers la droite
-                    child: Container(
-                      width: 100,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white, // Couleur du fond du sélecteur
-                      ),
-                      child: Center(
-                        child: DropdownButton<String>(
-                          value: _selectedUnit,
-                          onChanged: (newValue) {
-                            setState(() {
-                              _selectedUnit = newValue!;
-                            });
-                          },
-                          items: <String>['mmol/L', 'mg/dl'].map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(
-                                value,
-                                style: TextStyle(
-                                  color: Color(0xFF004396), // Couleur du texte
-                                ),
+                    child: Center(
+                      child: DropdownButton<String>(
+                        value: _selectedUnit,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedUnit = newValue!;
+                          });
+                        },
+                        items: <String>['mmol/L', 'mg/dl'].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(
+                                color: Color(0xFF004396), // Couleur du texte
                               ),
-                            );
-                          }).toList(),
-                        ),
+                            ),
+                          );
+                        }).toList(),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                Positioned(
+                  bottom: 80,
+                  left: 150,
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Color(0xFF004396),
+                    size: 45,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+
+          //fond bleu
+          SizedBox(height: 20), // Espace entre les deux blocs
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF004396), Color(0xFF0C8CE9)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+
+              // Autres widgets à l'intérieur de ce conteneur
+            ),
+          ),
+        ],
       ),
     );
   }
