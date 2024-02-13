32838a0aa7812c3a9bf1eec75ce5deaf346bfebb
CEO-Niels <nielsbeaumont.etu@gmail.com>
2024-02-13 11:22:59 +0100

    le selecteur est placer à droite, il reste du design a faire dessu ainsi que sur le graphique

--- lib/home/graphique.dart
+++ lib/home/graphique.dart
@@ -15,50 +15,55 @@ class _GlycemicChartState extends State<GlycemicChart> {
       children: [
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 20), // Ajout du padding horizontal
-          child: DropdownButton<bool>(
-            value: _showByDay,
-            onChanged: (newValue) {
-              setState(() {
-                _showByDay = newValue!;
-              });
-            },
-            underline: Container(), // Suppression du soulignement
-            style: TextStyle(
-              color: _showByDay ? Colors.white : Colors.black, // Couleur du texte en fonction de la sélection
-            ),
-            items: [
-              DropdownMenuItem(
-                value: true,
-                child: Container(
-                  color: _showByDay ? Color(0xFF004396) : Colors.white, // Fond du choix "Par jour"
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Par jour',
-                      style: TextStyle(color: _showByDay ? Colors.white : Colors.black), // Couleur du texte en fonction de la sélection
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end, // Alignement vers la droite
+            children: [
+              DropdownButton<bool>(
+                value: _showByDay,
+                onChanged: (newValue) {
+                  setState(() {
+                    _showByDay = newValue!;
+                  });
+                },
+                underline: Container(), // Suppression du soulignement
+                style: TextStyle(
+                  color: _showByDay ? Colors.white : Colors.black, // Couleur du texte en fonction de la sélection
+                ),
+                items: [
+                  DropdownMenuItem(
+                    value: true,
+                    child: Container(
+                      color: _showByDay ? Color(0xFF004396) : Colors.white, // Fond du choix "Par jour"
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Par jour',
+                          style: TextStyle(color: _showByDay ? Colors.white : Colors.black), // Couleur du texte en fonction de la sélection
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              DropdownMenuItem(
-                value: false,
-                child: Container(
-                  color: !_showByDay ? Color(0xFF004396) : Colors.white, // Fond du choix "Par heure"
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Par heure',
-                      style: TextStyle(color: !_showByDay ? Colors.white : Colors.black), // Couleur du texte en fonction de la sélection
+                  DropdownMenuItem(
+                    value: false,
+                    child: Container(
+                      color: !_showByDay ? Color(0xFF004396) : Colors.white, // Fond du choix "Par heure"
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Par heure',
+                          style: TextStyle(color: !_showByDay ? Colors.white : Colors.black), // Couleur du texte en fonction de la sélection
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
         ),
-        SizedBox(height: 20),
+        SizedBox(height: 0.5),
         AspectRatio(
-          aspectRatio: 1.3, // Réduction de la hauteur du graphique
+          aspectRatio: 1.8, // Réduction de la hauteur du graphique
           child: Container(
             margin: EdgeInsets.symmetric(horizontal: 20), // Ajout de marges horizontales
             decoration: BoxDecoration(
