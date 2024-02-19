ce7c2d7e07e7b23ee7dd044af02c74f3b966c0af
Claudemka <miakouikilapro@gmail.com>
2024-02-19 22:36:24 +0100

    fetch dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'auth/auth_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -78,14 +80,23 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.login),
+            tooltip: 'Connexion',
+            onPressed: () {
+              // Utilisez Navigator.push pour aller à la page d'authentification
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Authpage()),
+              );
+            },
+          ),
+        ],
       ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
